90b15ebed30f1d35b62ba512eba64d4055446251
Steve Pletcher <steve@bookbub.com>
2016-03-03 15:11:49 -0500

    More config shenanigans

--- hooks/boot_instance.sh
+++ hooks/boot_instance.sh
@@ -3,18 +3,17 @@
 #         whenever a new instance is provisioned. Updates to this are not
 #         automatically synced to AWS.
 # 1.) Install RVM and set ruby version
-sudo su -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-# relogin as root
-exit
+curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
+# relogin as root to set ruby version
 sudo su -
 # set default rvm to ruby 2.2.0
 rvm install ruby-2.2.0
 rvm --default ruby-2.2.0
-# add ubuntu to rvm group so they can run rvm
-usermod -a -G rvm ubuntu
 exit
+# add ubuntu to rvm group so they can run rvm
+sudo usermod -a -G rvm ubuntu
+sudo usermod -a -G rvm root
 # 2.) Install dependency managers and node
 gem install bundler
 sudo apt-get install nodejs
