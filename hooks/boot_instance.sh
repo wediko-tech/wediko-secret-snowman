708f89409d0b74664c1770e79809010b8bd7ecb9
Steve Pletcher <steve@bookbub.com>
2016-03-03 13:53:53 -0500

    Update to user data and deploy specs

--- hooks/boot_instance.sh
+++ hooks/boot_instance.sh
@@ -3,12 +3,18 @@
 #         whenever a new instance is provisioned. Updates to this are not
 #         automatically synced to AWS.
 # 1.) Install RVM and set ruby version
+sudo su -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/ubuntu/.rvm/scripts/rvm
+# relogin as root
+exit
+sudo su -
+# set default rvm to ruby 2.2.0
 rvm install ruby-2.2.0
 rvm --default ruby-2.2.0
-
+# add ubuntu to rvm group so they can run rvm
+usermod -a -G rvm ubuntu
+exit
 # 2.) Install dependency managers and node
 gem install bundler
 sudo apt-get install nodejs
