94cdb4ccdf4ce00e9a3abae796d2b3bb89614433
Steve Pletcher <steve@bookbub.com>
2016-03-03 19:47:58 -0500

    FINALLY

--- hooks/boot_instance.sh
+++ hooks/boot_instance.sh
@@ -3,28 +3,23 @@
 #         whenever a new instance is provisioned. Updates to this are not
 #         automatically synced to AWS.
 # 1.) Install rbenv and set ruby version
-sudo apt-get install git gcc make
+sudo apt-get -y install git gcc make
 git clone https://github.com/rbenv/rbenv.git /home/ubuntu/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/ubuntu/.bashrc
-bash -l
 echo "$(rbenv init -)" >> /home/ubuntu/.bashrc
 git clone https://github.com/garnieretienne/rvm-download.git /home/ubuntu/.rbenv/plugins/rvm-download
-# reset bash
-bash -l
 rbenv download 2.2.0
 # 2.) Install dependency managers and node
-sudo apt-get install bundler
-sudo apt-get install nodejs
+sudo apt-get -y install bundler
+sudo apt-get -y install nodejs
 
 # 3.) Install the AWS CLI to allow automated deploys
 sudo apt-get update
-sudo apt-get install python-pip
-sudo apt-get install ruby2.0
+sudo apt-get -y install python-pip
+sudo apt-get -y install ruby2.0
 sudo pip install awscli
 cd /home/ubuntu
 aws s3 cp s3://aws-codedeploy-us-west-2/latest/install . --region us-west-2
 chmod 755 install
 sudo ./install auto
 sudo service codedeploy-agent start
-exit
-exit
