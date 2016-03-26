e719aa1f6e67bcdc60edc230e43e34a92c79e580
Steve Pletcher <steve@bookbub.com>
2016-03-25 22:52:11 -0400

    Switch travis to local db

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
