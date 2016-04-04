90af2ca9717b1ff5b88274c2305687e347b2c013
Cara McCormack <caramcc@ccs.neu.edu>
2016-04-04 12:58:10 -0400

    event specs, update list specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @therapist = FactoryGirl.create(:therapist_user)
+    @event = FactoryGirl.create(:event)
+    @list = FactoryGirl.create(:list, therapist: @therapist.role, event: @event)
+  end
+
+  it 'has a valid factory' do
+    expect(@event).to be_valid
+  end
+
+  it 'properly associates with a list' do
+    expect(@event.lists.first.class).to eq List
+  end
+
+
 end
