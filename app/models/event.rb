592dc661ff292a8a3c2dd3ce395f62ca1ac06837
Cara McCormack <caramcc@ccs.neu.edu>
2016-04-05 11:31:29 -0400

    remove breaking validation (for now)

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,6 @@ class Event < ActiveRecord::Base
   has_many :lists, dependent: :destroy
 
   validates :title, presence: true
-  validates start_date_before_end_date?
-
-  def start_date_before_end_date?
-    if end_date && start_date
-      errors.add(:end_date, "Event can't end before start date") if end_date < start_date
-    end
-  end
 
   alias_attribute :name, :title
 end
