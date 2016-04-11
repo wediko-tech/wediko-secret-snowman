e9f6fc0354da3351a5166726a360dfe555a129cb
Steve Pletcher <steve@steve-pletcher.com>
2016-04-11 12:58:15 -0400

    Add events management to admin (#28)

---
+++ app/admin/events.rb
@@ -0,0 +1,60 @@
+ActiveAdmin.register Event do
+
+  index do
+    selectable_column
+    actions
+    column :title do |e|
+      link_to e.title, admin_event_path(e)
+    end
+    column :description do |e|
+      e.description.truncate(50, ommission: "...")
+    end
+    column :dates do |e|
+      "#{e.start_date.to_date} to #{e.end_date.to_date}"
+    end
+    column :active do |e|
+      status_tag(DateTime.current.between?(e.start_date, e.end_date))
+    end
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :start_date
+      row :end_date
+    end
+
+    if event.lists.any?
+      panel "Wishlists" do
+        table_for event.lists do
+          column :title do |wl|
+            link_to wl.title, admin_wishlist_path(wl)
+          end
+          column :therapist do |wl|
+            user = wl.therapist.user
+            link_to user.name, admin_user_path(user)
+          end
+          column :description do |wl|
+            wl.description.truncate(50, ommission: "...")
+          end
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs "Descriptors" do
+      f.input :title
+      f.input :description
+    end
+
+    f.inputs "Dates" do
+      f.input :start_date, as: :date_select
+      f.input :end_date, as: :date_select
+    end
+
+    f.actions
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,9 @@ class Event < ActiveRecord::Base
   validates :title, presence: true
   validate :start_date_before_end_date?
 
+  scope :active, -> { where("? BETWEEN start_date AND end_date", DateTime.current) }
+  scope :inactive, -> { where("? < start_date OR ? > end_date", DateTime.current, DateTime.current) }
+
   def start_date_before_end_date?
     if end_date && start_date
       errors.add(:end_date, "Event can't end before start date") if end_date < start_date
