ccfceb00c7bce2a32a1e0eeb53a81fab949e631b
Maelstroms <workpartystudy@yahoo.com>
2016-04-21 17:27:18 -0400

    committing in preparation of fixing PR comments

--- app/mailers/late_reservation_mailer.rb
+++ app/mailers/late_reservation_mailer.rb
@@ -1,6 +1,7 @@
 class LateReservationMailer < ApplicationMailer
-  def forgot_to_buy_gift_email(donor)
+  def forgot_to_buy_gift_email(donor, delinquencies)
     @donor = donor
+    @delinquencies = delinquencies
     @url  = login_url
     mail(to: @donor.email, subject: Rails.configuration.late_reservation_mailer_subject)
   end
--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -1,27 +1,29 @@
 require "spec_helper"
 RSpec.describe RegistrationMailer do
   describe 'notified registration' do
-    let(:user) { FactoryGirl.create(:donor_user) }
-    let(:mail) { RegistrationMailer.registration_email(user) }
+    before :each do
+      @user = FactoryGirl.create(:donor_user)
+      @mail = RegistrationMailer.registration_email(@user).deliver_now
+    end
 
       # Test the body of the sent email contains what we expect it to
     it 'renders the sender' do
-      expect(ApplicationMailer.default[:from]).to eql((mail.from)[0])
+      expect(ApplicationMailer.default[:from]).to eql((@mail.from)[0])
     end
     it 'addresses to proper recipient' do
-      expect((mail.to)[0]).to eql(user.email)
+      expect((@mail.to)[0]).to eql(@user.email)
     end
     it 'renders correct subject' do
-        expect(mail.subject).to eql(Rails.configuration.registration_mailer_subject)
+        expect(@mail.subject).to eql(Rails.configuration.registration_mailer_subject)
     end
     it 'renders correct body' do
-      expect(mail.body).to include("to login with your password at")
+      expect(@mail.body).to include("to login with your password at")
     end
     it 'renders users name properly' do
-      expect(mail.body.encoded).to include(user.name)
+      expect(@mail.body.encoded).to include(@user.name)
     end
-    it 'renders users name properly' do
-      expect(mail.body.encoded).to include(user.email)
+    it 'renders users email properly' do
+      expect(@mail.body.encoded).to include(@user.email)
     end
   end
 end
