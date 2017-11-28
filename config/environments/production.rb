97a6639841d60ab2b312e0b718b22f8bfc5d1aa7
Steve Pletcher <steve@bookbub.com>
2017-11-28 15:26:06 -0500

    Use correct URL in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  config.action_mailer.default_url_options = { host: 'wishlist.wediko.org' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
